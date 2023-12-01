c1426a32fca4c8167b8ce28a0168fd081f611190
Rahul Ramaswamy <36606911+rara1512@users.noreply.github.com>
2023-12-01 18:31:21 +0000

    scraped files added

--- HTML_Data/html_scraper.py
+++ HTML_Data/html_scraper.py
@@ -1,50 +1,39 @@
-import os
 import requests
+from bs4 import BeautifulSoup  
+from queue import Queue
 import lxml
-from bs4 import BeautifulSoup
 
-# Allowed domain 
-domain = "nyu.edu/"  
+START_URL = "https://www.nyu.edu/" 
+DOMAIN = "nyu.edu"
+  
+queue = Queue()
+scraped = set()  
 
-# Root folder to save text files
-root_dir = "NYU_Home"
-os.makedirs(root_dir, exist_ok=True)
+def scrape():
+    queue.put(START_URL)
 
-def scrape_site(url, domain):
-    try:
-        res = requests.get(url)
-    except: 
-        return  
-
-    soup = BeautifulSoup(res.text, 'lxml')
-    
-    # Get text content from paragraph tags
-    text = "" 
-    for paragraph in soup.find_all('p'):
-        text += paragraph.text + "\n"
-    
-    # Create file based on url 
-    file_name = url.replace("https://", "")
-    file_name = file_name.replace("/", "_")
-    file_path = os.path.join(root_dir, file_name + ".txt")
-    
-    # Save text content 
-    with open(file_path, "w") as f:
-        print(url, file=f)  
-        f.write(text)
+    while not queue.empty():
+        url = queue.get()
         
-    links = soup.find_all('a')
-    
-    # Follow subdomains
-    subdomains = []
-    for link in links:
-        href = link.get("href") # Returns None if not present
-        if href and domain in href:
-            subdomains.append(href)
-    
-    for url in subdomains:
-        scrape_site(url, domain)
-        
-# Start crawl
-base_url = "https://www."+domain
-scrape_site(base_url, domain)
+        if url in scraped:
+            continue
+            
+        scraped.add(url)
+            
+        print(f"Scraping: {url}")
+      
+        try:
+            response = requests.get(url)
+        except: 
+            continue 
+
+        soup = BeautifulSoup(response.content, 'lxml')
+        content = soup.find('div', {'class': 'content'}).text 
+
+        links = soup.find_all('a')
+        for link in links: 
+            href = link.get("href")  
+            if href and DOMAIN in href:
+                queue.put(href)
+
+scrape()
