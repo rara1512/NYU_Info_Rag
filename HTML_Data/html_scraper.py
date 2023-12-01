5129b7b8d66e0eea4db8e9169acb2fd7080173f7
Rahul Ramaswamy <36606911+rara1512@users.noreply.github.com>
2023-12-01 15:25:47 +0000

    scraper changed

--- HTML_Data/html_scraper.py
+++ HTML_Data/html_scraper.py
@@ -1,41 +1,45 @@
 import os
 import requests
-from urllib.parse import urlparse, urljoin
 from bs4 import BeautifulSoup
-import tldextract
 
-def save_page(url, soup, base_path):
-    domain_name = tldextract.extract(url).domain
-    parsed_url = urlparse(url)
-    filename = os.path.join(base_path, domain_name, parsed_url.path.lstrip("/").replace("/", "_"))
+# Allowed domain 
+domain = "nyu.edu/"  
 
-    os.makedirs(os.path.dirname(filename), exist_ok=True)
+# Root folder to save text files
+root_dir = "NYU_Home"
+os.makedirs(root_dir, exist_ok=True)
 
-    with open(filename + ".txt", 'w', encoding='utf-8') as f:
-        f.write(soup.get_text())
+def scrape_site(url, domain):
+    try:
+        res = requests.get(url)
+    except: 
+        return  
 
-def scrape_website(starting_url, base_path):
-    stack = [starting_url]
-
-    while stack:
-        current_url = stack.pop()
-
-        response = requests.get(current_url)
-
-        if response.status_code == 200:
-            soup = BeautifulSoup(response.text, 'html.parser')
-            save_page(current_url, soup, base_path)
-
-            links = soup.find_all('a', href=True)
-
-            for link in links:
-                absolute_url = urljoin(current_url, link['href'])
-
-                if urlparse(absolute_url).netloc.endswith('nyu.edu'):
-                    stack.append(absolute_url)
-
-if __name__ == "__main__":
-    starting_url = "https://www.nyu.edu/"
-    base_save_path = "NYU Home"
-
-    scrape_website(starting_url, base_save_path)
+    soup = BeautifulSoup(res.text, 'lxml')
+    
+    # Get text content from paragraph tags
+    text = "" 
+    for paragraph in soup.find_all('p'):
+        text += paragraph.text + "\n"
+    
+    # Create file based on url 
+    file_name = url.replace("https://", "")
+    file_name = file_name.replace("/", "_")
+    file_path = os.path.join(root_dir, file_name + ".txt")
+    
+    # Save text content 
+    with open(file_path, "w") as f:
+        print(url, file=f)  
+        f.write(text)
+        
+    links = soup.find_all('a')
+    
+    # Follow subdomains
+    subdomains = [link["href"] for link in links if domain in link["href"]] 
+    
+    for url in subdomains:
+        scrape_site(url, domain)
+        
+# Start crawl
+base_url = "https://www."+domain
+scrape_site(base_url, domain)
